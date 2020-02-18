c83cb1b24ca8f78c4619d20af9193c5304d47dce
syspashtet <48693342+syspashtet@users.noreply.github.com>
2020-02-18 23:50:25 +0300

    Create shawarma.sh

---
+++ shawarma.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#made by syspashtet for Rebrain Task GIT 4
+echo "Hello my Friend! Please input what do you want to eat or how much money
+you have and i recommend food or food-place for you!"
+read food
+echo -e "\e[31mGO TO SHAWARMA MY FRIEND!\e[0m"
+exit
