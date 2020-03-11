5cc3cf3591f25a8febcc88be2267c6615f918f7f
syspashtet <48693342+syspashtet@users.noreply.github.com>
2020-03-12 00:23:15 +0300

    Update shawarma.sh

--- shawarma.sh
+++ shawarma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #made by syspashtet for Rebrain Task GIT 4
-echo "Hello my Friend! Please input what do you want to eat or how much money
-you have and i recommend food or food-place for you!"
+echo "\e[36mHello my Friend! Please input what do you want to eat or how much money
+you have and i recommend food or food-place for you!\e[0m"
 read food
 echo -e "\e[31mGO TO SHAWARMA MY FRIEND!\e[0m"
 exit
